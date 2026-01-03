010f7737191bf640b01e33c8ce09c5d521ebaac7
globnot <antoineborda-pro@proton.me>
2026-01-03 13:34:06 +0100

    Refacto operations_swap.c

--- srcs/operations_swap.c
+++ srcs/operations_swap.c
@@ -6,39 +6,39 @@
 /*   By: aborda <aborda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/03 11:36:15 by aborda            #+#    #+#             */
-/*   Updated: 2026/01/03 13:15:38 by aborda           ###   ########.fr       */
+/*   Updated: 2026/01/03 13:33:07 by aborda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	sa(t_node **stack_a)
+void	swap(t_node **stack)
 {
 	int	tmp;
 
-	if ((*stack_a == NULL) || ((*stack_a)->next == NULL))
+	if ((*stack == NULL) || ((*stack)->next == NULL))
 		return ;
-	tmp = (*stack_a)->value;
-	(*stack_a)->value = (*stack_a)->next->value;
-	(*stack_a)->next->value = tmp;
+	tmp = (*stack)->value;
+	(*stack)->value = (*stack)->next->value;
+	(*stack)->next->value = tmp;
+
+}
+
+void	sa(t_node **stack_a)
+{
+	swap(stack_a);
 	ft_printf("sa\n");
 }
 
 void	sb(t_node **stack_b)
 {
-	int	tmp;
-
-	if ((*stack_b == NULL) || ((*stack_b)->next == NULL))
-		return ;
-	tmp = (*stack_b)->value;
-	(*stack_b)->value = (*stack_b)->next->value;
-	(*stack_b)->next->value = tmp;
+	swap(stack_b);
 	ft_printf("sb\n");
 }
 
 void	ss(t_node **stack_a, t_node **stack_b)
 {
-	sa(&(*stack_a));
-	sb(&(*stack_b));
+	swap(stack_a);
+	swap(stack_b);
+	ft_printf("ss\n");
 }
-
