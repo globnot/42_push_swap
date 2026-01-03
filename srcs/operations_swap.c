beaf9fa70776aeb5f369d0155859dea14a6d8a76
globnot <antoineborda-pro@proton.me>
2026-01-03 11:58:11 +0100

    Added operations_swap.c

---
+++ srcs/operations_swap.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   operations_swap.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aborda <aborda@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/03 11:36:15 by aborda            #+#    #+#             */
+/*   Updated: 2026/01/03 11:57:32 by aborda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	sa(t_node **stack_a)
+{
+	int	tmp;
+
+	if ((*stack_a == NULL) || ((*stack_a)->next == NULL))
+		return;
+	if ((*stack_a)->next == NULL)
+		return;
+	else
+	{
+		tmp = (*stack_a)->value;
+		(*stack_a)->value = (*stack_a)->next->value;
+		(*stack_a)->next->value = tmp;
+		ft_printf("sa\n");
+	}
+}
